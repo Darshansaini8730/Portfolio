8e76852efab7e947157bb65f3b4a6dde54ce32f3
Darshansaini8730 <darshansaini8730@gmail.com>
2025-09-08 20:26:54 +0530

    Added installtion to

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  plugins: [viteCompression()],
+  plugins: [visualizer({ open: true })],
+  treeshake: true,
 })
